be600cb1652b58daea1b34d959b92d4f259cd62d
Seiji Umatani <umatani@gmail.com>
2025-02-17 10:01:27 +0900

    add deps to rackunit

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection "lightstep")
-(define deps '("base"))
+(define deps '("base" "rackunit-lib"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/lightstep.scrbl" ())))
 (define pkg-desc "Lightweight Reduction Engine")
