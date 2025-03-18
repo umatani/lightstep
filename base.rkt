89ce201ad0628dac47573ae5822e247e49a71a91
Seiji Umatani <umatani@gmail.com>
2025-03-18 13:58:55 +0900

    bug fix

--- base.rkt
+++ base.rkt
@@ -3,23 +3,17 @@
 (require (rename-in (prefix-in set "set.rkt")
                     [set-make    set]
                     [set-for/set for/set]))
-(define ∅       set-∅)
-(define ∅?      set-∅?)
-(define ∈       set-∈)
-(define ∪       set-∪)
-(define ⊆       set-⊆)
-(define in-set  set-in-set)
-(provide (all-from-out "set.rkt") set ∅ ∅? ∈ ∪ ⊆ for/set in-set)
+(provide (all-from-out "set.rkt")
+         (rename-out [set-∅ ∅] [set-∅? ∅?]
+                     [set-∈ ∈] [set-∪ ∪] [set-⊆ ⊆] [set-in-set in-set]))
 
 (require (rename-in (prefix-in map "map.rkt")
                     [map-make ↦]
                     [map-for/map for/map]))
-(define dom      map-dom)
-(define rng      map-rng)
-(define ⊔        map-∪)
-(define restrict map-restrict)
-(define in-map   map-in-map)
-(provide (all-from-out "map.rkt") ↦ dom rng ⊔ restrict for/map in-map)
+(provide (all-from-out "map.rkt")
+         (rename-out [map-dom dom] [map-rng rng] [map-∪ ⊔]
+                     [map-restrict restrict]
+                     [map-in-map in-map]))
 
 
 (require "match.rkt")
